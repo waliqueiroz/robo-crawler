5f13d8070f9bbeda09a3e67638bfeb5072962df3
Wali Queiroz <wsantos077@gmail.com>
2019-11-01 15:33:29 -0300

    Correções em variáveis

--- src/services/AppService.js
+++ src/services/AppService.js
@@ -2,27 +2,27 @@ const puppeteer = require('puppeteer');
 const cheerio = require('cheerio');
 module.exports = {
     async buscar(url) {
-        let response = [];
+        const response = [];
         const browser = await puppeteer.launch();
         const page = await browser.newPage();
         await page.goto(url);
 
-        let content = await page.content();
-        let $ = cheerio.load(content);
+        const content = await page.content();
+        const $ = cheerio.load(content);
 
         $('.roomName').each(function () {
-            let nome = $(this).find('.excerpt  h5  a').text().trim();
-            let descricao = $(this).find('.excerpt .description').text().trim();
-            let preco = $(this).find('.sincePrice h6').text().trim();
+            const nome = $(this).find('.excerpt h5 a').text().trim();
+            const descricao = $(this).find('.excerpt .description').text().trim();
+            const preco = $(this).find('.sincePrice h6').text().trim();
 
-            let imagens = [];
+            const imagens = [];
 
             $(this).find('.roomSlider .slide').each(function () {
                 const imagemURL = 'https://myreservations.omnibees.com' + $(this).find('img').attr('src');
                 imagens.push(imagemURL);
             });
 
-            let item = {
+            const item = {
                 nome,
                 descricao,
                 preco,
@@ -31,6 +31,8 @@ module.exports = {
 
             response.push(item);
         });
+
+        browser.close();
 
         return response;
     }
